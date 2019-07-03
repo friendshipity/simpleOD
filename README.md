113bc861e75f661658f55e2ff502fe2c8d9e703b
yyq90 <420936401@qq.com>
2019-07-03 16:51:15 +0800

    test

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # simpleOD
-simple
+simple  
+大图
+![image](https://github.com/yyq90/simpleOD/raw/master/images/nongshalie.jpg)
+
+
+使用的方法  
+相似度的衡量
+根据小图T的尺寸，通过滑动窗口检测大图切片I(x+x',y+y')(下面用I'表示)，针对小图所有x',y',检测其和T(x‘,y’)是否都相等。
+
+
+相似度
+#### 1.array match  
+result：
+缺点，只是检测到和小图像素值完全一样的区域。
+#### 2.template match  
+类似1,计算T和I'
+是cv2.matchTemplate(imageGray,boxGray,cv2.TM_CCOEFF)的具体实现。
+缺点，仍然为template matching，泛化能力低，只能取图中与小图相同的框。但是相较于方法1，允许template像素值于I'的不同
+#### 3.pixal iou
+如果需要检测相同颜色的飞机。可以给I'和T设定阈值，保留阈值以上形成mask,在计算所有通道mask I'和mask T的iou值。
+
+#### 4.ssim  
+类似1，计算T和I'的ssim(structural similarity)Index，亮度、对比度和结构
+
+ssim效果较好。
+得到每个滑动窗口的得分后，取score排序top200的框.用score,坐标的x,y三个feature进行kmeans聚类（ssim的score,小于1，可以放大score，这样可以加强score feature的权重）。
+取score mean最高的类中的box作为最后的输出。
+
+讨论:  
+1. 在此任务中，使用聚类这种方法可能会导致的泛化能力的降低(比如需要检测不同动作和颜色的所有飞机)。这个时候可以考虑使用nms对score top100个框进行处理。
+2. 不同于直接计算T和I'的相似度，可以用hog，orb方法提取feature进行比较或者训练。
---
+++ match.py
@@ -0,0 +1,91 @@
+import numpy as np
+import cv2
+import sys
+from skimage.measure import compare_ssim as ssim
+
+
+def raw_method(img, template):
+    A = img == template
+    return A.all()
+
+
+def cross_coefficient(img, template):
+    imgGrey = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    templateGrey = cv2.cvtColor(template, cv2.COLOR_RGB2GRAY)
+    return ((imgGrey - imgGrey.mean()) * (templateGrey - templateGrey.mean())).sum()
+
+
+def pixel_threshold_iou(img, template, p=0.3):
+    img = img.astype(np.float32) / 255
+    template = template.astype(np.float32) / 255
+    img[img < p] = 0
+    template[template < p] = 0
+
+    ious = []
+    for i in range(3):
+        intersection = np.logical_and(img[i], template[i]).sum()
+        union = np.logical_or(img[i], template[i]).sum()
+        if union != 0:
+            ious.append(intersection / union)
+    return np.array(ious).mean()
+
+
+def SSIM(img, template):
+    return ssim(img, template, multichannel=True)
+
+
+positions = []
+img = cv2.imread(sys.argv[1])
+box = cv2.imread(sys.argv[2])
+
+X, Y, _ = np.shape(img)
+bx, by, _ = np.shape(box)
+for i in range(0, X - bx, 1):
+    for j in range(0, Y - by, 1):
+        positions.append((raw_method(img[i:i + bx, j:j + by, :], box) + 0, i, j))
+sorted = sorted(positions, key=lambda k: k[0])[-200:]
+
+from sklearn.cluster import KMeans
+
+cluster_num = 2
+
+kmeans = KMeans(n_clusters=cluster_num)
+w = np.array(sorted)
+w[:, 0] = w[:, 0] * 1000
+y = kmeans.fit(w)
+pred = np.hstack((y.labels_.reshape(200, 1), np.array(sorted)))
+
+max_mean = 0
+max_index = None
+for l in range(cluster_num):
+    mean = pred[pred[:, 0] == l][:, 1].mean()
+    if mean > max_mean:
+        max_mean = mean
+        max_index = l
+for v in pred[pred[:, 0] == max_index]:
+    cv2.rectangle(img, (int(v[3]), int(v[2])), (int(v[3]) + by, int(v[2]) + bx), (0, 255, 0), 1)
+
+# font = cv2.FONT_HERSHEY_SIMPLEX
+# for index,v in enumerate(sorted[-150:]):
+#     if not v[0]>0:
+#         continue
+#     label = y_pred.labels_[index+50]
+#     if label == 0:
+#         cv2.rectangle(img, (v[2], v[1]), (v[2] + by, v[1] + bx), (255, 0, 0), 1)
+#
+#     elif label ==1:
+#         cv2.rectangle(img, (v[2], v[1]), (v[2] + by, v[1] + bx), (0, 255, 0), 1)
+#
+#     elif label == 2:
+#         cv2.rectangle(img, (v[2], v[1]), (v[2] + by, v[1] + bx), (0, 0, 255), 1)
+#
+#     elif label == 3:
+#         cv2.rectangle(img, (v[2], v[1]), (v[2] + by, v[1] + bx), (255, 255, 0), 1)
+#
+#     elif label ==4 :
+#         cv2.rectangle(img, (v[2], v[1]), (v[2] + by, v[1] + bx), (0, 255, 255), 1)
+#
+#     # cv2.putText(img, (v[2], v[1]), (v[2] + by, v[1] + bx), (255, 255, 255), 1)
+#     cv2.putText(img, '%.2f' % v[0], (v[2], v[1]), font, 0.2, (0, 255, 0), 1)
+# bbox.append((v[2],v[1],v[2]+by,v[1]+bx))
+cv2.imwrite('det_raw_crop_2.png', img)
